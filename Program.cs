3562f2fb3a36df37633017f2b55136c34276f113
Evgeny Ivchenko <goldenjoe@outlook.com>
2021-01-26 18:22:20 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+const int arraySize = 64;
+IntPtr src = Marshal.AllocHGlobal(arraySize * sizeof(int));
+unsafe
+{
+    new Span<int>(src.ToPointer(), arraySize).Fill(42);
+}
+var dst = new int[arraySize];
+Vortice.Interop.Read(src, dst);
+unsafe
+{
+    Debug.Assert(new Span<int>(src.ToPointer(), arraySize).SequenceEqual(dst));
+}
